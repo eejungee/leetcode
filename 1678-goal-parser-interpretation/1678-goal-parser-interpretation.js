975cac999848b5be4f868955bd4e7326fe6af762
eejungee <88709727+eejungee@users.noreply.github.com>
2024-09-11 12:28:44 +0900

    Time: 48 ms (78.77%), Space: 49.1 MB (41.61%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    let result = []
+    console.log(command.length)
+    for(let i=0; i<command.length; i++) {
+        if(command[i] === "G") {
+            result.push(command[i])
+        } else if(command[i] === "(") {
+            if(command[i+1] === ")") {
+                result.push("o")
+                i++
+            }else if(command[i+1] === "a") {
+                result.push("al")
+                i += 3
+            }
+        } 
+    }
+    return result.join("")
+};
